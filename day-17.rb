3f5af3a3b39d6e6a1f767a214c5177a0bd474dcb
Mike Busch <mbusch@heroku.com>
2017-12-17 08:22:40 -0500

    day 17 refactorings

--- day-17.rb
+++ day-17.rb
@@ -1,66 +1,55 @@
 require "rspec"
 class Spinlock
-  attr_reader :seed, :pos, :array, :length
-  alias_method :to_a, :array
+  attr_reader :seed, :pos, :value, :length
 
   def initialize(seed:)
     @seed = seed
-    @array = [0]
     @pos = 0
-    @next_value = 1
+    @value = 0
     @length = 1
   end
 
   def next(&block)
     @pos = ((pos + seed) % @length) + 1
-    if block_given?
-      block.call(pos, next_value)
-    else
-      array.insert(pos, next_value)
-    end
+    @value += 1
     @length += 1
-    @next_value += 1
-    self
+    [pos, value]
   end
-
-  private
-  attr_reader :next_value
 end
 describe Spinlock do
   let(:spinlock) { described_class.new(seed: 3) }
 
-  specify "starts with a single element, value of 0" do
-    expect(spinlock.to_a).to eq [0]
-  end
-
   describe "next" do
-    specify "inserts values" do
-      expect(spinlock.next.to_a).to eq [0, 1]
-      expect(spinlock.next.to_a).to eq [0, 2, 1]
-      expect(spinlock.next.to_a).to eq [0, 2, 3, 1]
-      expect(spinlock.next.to_a).to eq [0, 2, 4, 3, 1]
-      expect(spinlock.next.to_a).to eq [0, 5, 2, 4, 3, 1]
-      expect(spinlock.next.to_a).to eq [0, 5, 2, 4, 3, 6, 1]
-      expect(spinlock.next.to_a).to eq [0, 5, 7, 2, 4, 3, 6, 1]
-      expect(spinlock.next.to_a).to eq [0, 5, 7, 2, 4, 3, 8, 6, 1]
-      expect(spinlock.next.to_a).to eq [0, 9, 5, 7, 2, 4, 3, 8, 6, 1]
+    specify "returns position and value" do
+      expect(spinlock.next).to eq [1, 1]
+      expect(spinlock.next).to eq [1, 2]
+      expect(spinlock.next).to eq [2, 3]
+      expect(spinlock.next).to eq [2, 4]
+      expect(spinlock.next).to eq [1, 5]
+      expect(spinlock.next).to eq [5, 6]
+      expect(spinlock.next).to eq [2, 7]
+      expect(spinlock.next).to eq [6, 8]
+      expect(spinlock.next).to eq [1, 9]
     end
   end
 end
 
+return if /rspec$/ === $PROGRAM_NAME
+
 # Part 1
 spinlock = Spinlock.new(seed: 329)
-2017.times { spinlock.next }
-puts spinlock.array[spinlock.pos + 1]
+array = [0]
+2017.times do
+  array.insert(*spinlock.next)
+end
+puts array[spinlock.pos + 1]
 
 # Part 2
 spinlock = Spinlock.new(seed: 329)
 
 last_value = nil
 50_000_000.times do |i|
-  spinlock.next do |pos, value|
-    last_value = value if pos == 1
-  end
+  pos, value = spinlock.next
+  last_value = value if pos == 1
 end
-
 puts last_value
