e0edd54a845d1e00ef397b6235e37feffe52251a
Mike Busch <mbusch@heroku.com>
2017-12-22 02:24:23 -0500

    day 22... such refactoring. very wow.

--- day-22.rb
+++ day-22.rb
@@ -2,60 +2,56 @@ require "rspec"
 require "set"
 
 class SporificaVirus
-  attr_reader :current_node, :cluster, :bursts, :infections
+  attr_reader :cluster, :burst_count, :infections_caused
+  attr_accessor :current_node
 
   def initialize(cluster)
-    @cluster = populate_cluster(cluster.split(/\n/).map(&:chars))
+    @cluster = populate_cluster(cluster)
     @current_node = [0, 0]
     @direction = 0
-    @bursts = 0
-    @infections = 0
+    @burst_count = 0
+    @infections_caused = 0
   end
 
   def burst
     turn
     work
     move
-    @bursts += 1
+    @burst_count += 1
     self
   end
 
   def turn
-    if infected?(current_node)
-      @direction += 1
-    else
-      @direction -= 1
+    case state(current_node)
+    when :infected then @direction += 1
+    when :clean    then @direction -= 1
     end
   end
 
   def work
-    if infected?(current_node)
-      clean
-    else
-      infect
+    case state(current_node)
+    when :infected then clean
+    when :clean    then infect
+    end
+  end
+
+  def move
+    x, y = current_node
+    case direction
+    when :right then self.current_node = [x + 1, y]
+    when :left  then self.current_node = [x - 1, y]
+    when :up    then self.current_node = [x, y + 1]
+    when :down  then self.current_node = [x, y - 1]
     end
   end
 
   def infect
-    @infections += 1
-    cluster.store(current_node.dup, :infected)
+    @infections_caused += 1
+    cluster.store(current_node, :infected)
   end
 
   def clean
     cluster.delete(current_node)
-  end
-
-  def move
-    case direction
-    when :up
-      current_node[1] += 1
-    when :right
-      current_node[0] += 1
-    when :down
-      current_node[1] -= 1
-    when :left
-      current_node[0] -= 1
-    end
   end
 
   DIRECTIONS = [:up, :right, :down, :left]
@@ -67,116 +63,126 @@ class SporificaVirus
     cluster.fetch(pos, :clean)
   end
 
-  def infected?(pos)
-    state(pos) == :infected
-  end
-
   private
 
+  INFECTED_MARKER = "#".freeze
+  # parse the given cluster input string into a hash of infected cells
+  #
+  # For example:
+  #
+  # populate_cluster(<<~CLUSTER)
+  #   ..#
+  #   #..
+  #   ...
+  # CLUSTER
+  #
+  # would return:
+  # {[-1, 0] => :infected, [1, 1] => :infected}
+  #
   def populate_cluster(cluster)
+    cluster = cluster.split(/\n/).map(&:chars)
     midx = cluster.first.length / 2
     midy = cluster.length / 2
-    infected_cells = {}
-    cluster.each_with_index do |row, y|
+
+    cluster.each_with_object({}).with_index do |(row, cluster), y|
       row.each_with_index do |cell, x|
-        if cell == "#"
-          infected_cells.store([x - midx, midy - y], :infected)
-        end
+        # create the code adjusting x and y based on the center of the given
+        # cluster being [0, 0]
+        node = [x - midx, midy - y]
+
+        # store the node only if it's infected
+        cluster.store(node, :infected) if cell == INFECTED_MARKER
       end
     end
-    infected_cells
   end
 end
 
 describe SporificaVirus do
-  specify "the example puzzle" do
-    cluster = <<~CLUSTER
+  let(:virus) do
+    SporificaVirus.new(<<~CLUSTER)
       ..#
       #..
       ...
     CLUSTER
+  end
 
-    virus = SporificaVirus.new(cluster)
+  specify "the example puzzle" do
     expect(virus.current_node).to eq [0, 0]
     expect(virus.direction).to eq :up
 
     virus.burst
-    expect(virus.infected?([0, 0])).to eq true
+    expect(virus.state([0, 0])).to be :infected
     expect(virus.direction).to eq :left
     expect(virus.current_node).to eq [-1, 0]
 
     virus.burst
-    expect(virus.infected?([-1, 0])).to eq false
+    expect(virus.state([-1, 0])).to be :clean
     expect(virus.direction).to eq :up
     expect(virus.current_node).to eq [-1, 1]
 
     4.times { virus.burst }
-    expect(virus.infected?([-1, 0])).to eq true
-    expect(virus.infected?([-2, 0])).to eq true
-    expect(virus.infected?([-2, 1])).to eq true
-    expect(virus.infected?([-1, 1])).to eq true
+    expect(virus.state([-1, 0])).to be :infected
+    expect(virus.state([-2, 0])).to be :infected
+    expect(virus.state([-2, 1])).to be :infected
+    expect(virus.state([-1, 1])).to be :infected
     expect(virus.direction).to eq :up
     expect(virus.current_node).to eq [-1, 1]
 
     virus.burst
-    expect(virus.infected?([-1, 1])).to eq false
+    expect(virus.state([-1, 1])).to be :clean
     expect(virus.direction).to eq :right
     expect(virus.current_node).to eq [0, 1]
 
-    expect(virus.bursts).to eq 7
-    expect(virus.infections).to eq 5
+    expect(virus.burst_count).to eq 7
+    expect(virus.infections_caused).to eq 5
 
-    virus.burst until virus.bursts == 70
-    expect(virus.infections).to eq 41
+    virus.burst until virus.burst_count == 70
+    expect(virus.infections_caused).to eq 41
+  end
 
-    virus.burst until virus.bursts == 10_000
-    expect(virus.infections).to eq 5587
+  specify "10_000 times", slow: true do
+    virus.burst until virus.burst_count == 10_000
+    expect(virus.infections_caused).to eq 5587
   end
 end
 
 class SporificaVirusDeux < SporificaVirus
   def turn
     case state(current_node)
-    when :infected
-      @direction += 1
-    when :clean
-      @direction -= 1
-    when :flagged
-      @direction += 2
+    when :infected then @direction += 1
+    when :clean    then @direction -= 1
+    when :flagged  then @direction += 2
     end
   end
 
   def work
     case state(current_node)
-    when :clean
-      weaken
-    when :weakened
-      infect
-    when :infected
-      flag
-    when :flagged
-      clean
+    when :clean    then weaken
+    when :weakened then infect
+    when :infected then flag
+    when :flagged  then clean
     end
   end
 
   def weaken
-    cluster.store(current_node.dup, :weakened)
+    cluster.store(current_node, :weakened)
   end
 
   def flag
-    cluster.store(current_node.dup, :flagged)
+    cluster.store(current_node, :flagged)
   end
 end
 
 describe SporificaVirusDeux do
-  specify "the example" do
-    cluster = <<~CLUSTER
+  let(:virus) do
+    described_class.new(<<~CLUSTER)
       ..#
       #..
       ...
     CLUSTER
+  end
 
-    virus = described_class.new(cluster)
+  specify "the example scenario" do
     expect(virus.current_node).to eq [0, 0]
     expect(virus.direction).to eq :up
 
@@ -205,11 +211,13 @@ describe SporificaVirusDeux do
     expect(virus.state([-2, 0])).to eq :infected
     expect(virus.direction).to eq :left
 
-    virus.burst until virus.bursts == 100
-    expect(virus.infections).to eq 26
+    virus.burst until virus.burst_count == 100
+    expect(virus.infections_caused).to eq 26
+  end
 
-    virus.burst until virus.bursts == 10_000_000
-    expect(virus.infections).to eq 2_511_944
+  specify "10_000_000 times", slow: true do
+    virus.burst until virus.burst_count == 10_000_000
+    expect(virus.infections_caused).to eq 2_511_944
   end
 end
 
@@ -246,11 +254,11 @@ CLUSTER
 # Part 1
 virus = SporificaVirus.new(cluster)
 
-virus.burst until virus.bursts == 10_000
-puts virus.infections
+virus.burst until virus.burst_count == 10_000
+puts virus.infections_caused
 
 # Part 2
 evolved_virus = SporificaVirusDeux.new(cluster)
 
-evolved_virus.burst until evolved_virus.bursts == 10_000_000
-puts evolved_virus.infections
+evolved_virus.burst until evolved_virus.burst_count == 10_000_000
+puts evolved_virus.infections_caused
